2bb8133b80d80b8f3b5d192dc2c846d4fe4c01e2
anand-joshi-sp <anand.joshi@sailpoint.com>
2025-09-01 15:30:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,5 +13,5 @@ def ssf():
     return "OK", 200
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))  # Render provides PORT env
+    port = int(os.environ.get("PORT", 5000))  # Render provides $PORT
     app.run(host="0.0.0.0", port=port)
